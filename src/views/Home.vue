5641a8099c7ff26cd2bf484101220a11db676a52
邬绪威 <wxw@24e.co>
2020-03-25 10:37:29 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map" >
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  mounted() {
+    this.mapInit();
+  },
+  methods:{
+    mapInit(){
+      const viewer = new Cesium.Viewer('map')
+      const numberOfBalloons = 13;
+      const lonIncrement = 0.00025;
+      const initialLon = -122.99875;
+      const lat = 44.0503706;
+      const height = 100;
+      const url = 'http://192.168.1.242:3000/model/CesiumAir/Cesium_Air.glb';
+      for (let i = 0; i < numberOfBalloons; ++i) {
+        let lon = initialLon + i * lonIncrement;
+        createModel(url, lon, lat, height);
+      }
+      const target = Cesium.Cartesian3.fromDegrees(initialLon + lonIncrement, lat, height + 7.5);
+      const offset = new Cesium.Cartesian3(-37.048378684557974, -24.852967044804245, 4.352023653686047);
+      viewer.scene.camera.lookAt(target, offset);
+      function createModel(url, x, y, height) {
+        const position = Cesium.Cartesian3.fromDegrees(x, y, height);
+        viewer.entities.add({
+          name : url,
+          position : position,
+          model : {
+            uri : url
+          }
+        });
+      }
+    }
   }
 }
 </script>
